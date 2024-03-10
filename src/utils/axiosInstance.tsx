ac96e4ccc96b245bff47807399dcb4b72f1bff3b
abdrasaq14 <abdrasaq14@gmail.com>
2024-03-10 22:25:26 +0100

    added the backend URL

--- src/utils/axiosInstance.tsx
+++ src/utils/axiosInstance.tsx
@@ -1,16 +1,14 @@
 import axios from "axios";
 
 // backend url
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const axiosInstance = axios.create({
-  baseURL: BACKEND_URL,
+  baseURL: "https://traidr-backend.onrender.com",
   withCredentials: true,
 });
 
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
-    console.log("backend url", BACKEND_URL);
     if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
     }
